ac9a7182de90484c41f6504361ef1c486121f58e
Dominique Castaneda <dominiquecastaneda@yahoo.com>
2022-07-19 17:10:33 -0500

    publish to github

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,13 +8,13 @@ function HomeScreen() {
       <div style={{marginTop: 35, textAlign: "center"}}>
         <img src='/images/decorativelily.JPG' alt="Decorativelily" height="auto" width="100%" />
       </div>
-      <div style={{textAlign: "center", marginTop: 50, color: "#ff69b4", fontSize: 50}}>
+      <div style={{textAlign: "center", marginTop: 50, color: "#ff69b4", fontSize: 35}}>
        Our Story
        <div style={{marginTop: 5, fontSize: 25}}>
            Decorativelily was created in 2022 after one of my son's became engaged. He requested my help in finding special ways to propose to his beautiful girlfriend at an out of state beach. Since I knew my hands were tied with not being able to take any of my handmade products on the plane, we had to look for event planners to help us plan it and thats when we came across a family owned company who set up picnics at the beach. With their help, we were able to make his proposal extra special. It was such a beautiful picture perfect setting and I knew that I wanted to do something similar here in the DFW area to help people plan out their memorable moments. My love of entertaining, crafting, sewing, and recently new woodworking hobby, came to play by me making my own products from picnic tables, sewing pillow covers, signs, etc. There was no question that this was a great business idea for me as I would love to help make great memories for my customers.
        </div>
       </div>
-      <div style={{textAlign: "center", color: "#ff69b4", marginTop: 50, fontSize: 50}}>
+      <div style={{textAlign: "center", color: "#ff69b4", marginTop: 50, fontSize: 35}}>
           Packages
           <div style={{textAlign: "center", color: "#ff69b4", fontSize: 25, marginTop: 10}}>
             Picnic For 2
@@ -29,7 +29,7 @@ function HomeScreen() {
             Picnic For 9-12
           </div>
       </div>
-      <div style={{textAlign: "center", color: "#ff69b4", marginTop: 50, fontSize: 50}}>
+      <div style={{textAlign: "center", color: "#ff69b4", marginTop: 50, fontSize: 35}}>
           FAQ 
           <div style={{marginTop: 10, fontSize: 25}}>
             HOW DO I BOOK A PICNIC?
@@ -50,7 +50,7 @@ function HomeScreen() {
           If you need to reschedule your picnic due to weather or other unforseen circumstances, we can apply your funds to another date within the calendar year. We do require 24 hours notice upon rescheduling due to weather.
           </div>
       </div>
-        <div style={{textAlign: "center", marginTop: 50, color: "#ff69b4", fontSize: 50}}>
+        <div style={{textAlign: "center", marginTop: 50, color: "#ff69b4", fontSize: 35}}>
             Picnic Booking Request
         </div>
         <div>
@@ -111,8 +111,14 @@ function HomeScreen() {
             <a href="https://www.instagram.com/decorativelily" class="btn social-icon button" data-abc="true">
                 <i style={{color: "#C13584", fontSize: 25, paddingRight: 15}} class="fa fa-instagram btn-round fa-lg"></i>
             </a>
-            <a href="https://www.youtube.com/channel/" class="btn social-icon button" data-abc="true">
-              <i style={{color: "#C13584", fontSize: 25}} class="fa fa-youtube btn-round fa-lg"></i>
+            <a href="https://www.snapchat.com/decorativelily" class="btn social-icon button" data-abc="true">
+                <i style={{color: "#C13584", fontSize: 25, paddingRight: 15}} class="fa fa-snapchat btn-round fa-lg"></i>
+            </a>
+            <a href="https://www.youtube.com/channel/UClslCAFh26m4yGcviIGgDhQ" class="btn social-icon button" data-abc="true">
+              <i style={{color: "#C13584", fontSize: 25, paddingRight: 15}} class="fa fa-youtube btn-round fa-lg"></i>
+            </a>
+            <a href="https://www.tiktok.com/@decorativelily" class="" data-abc="">
+                <img src="/images/tiktok.JPG" alt="Decorativelily TikTok" style={{height: 25}}/>
             </a>
         </div>
       </div>
