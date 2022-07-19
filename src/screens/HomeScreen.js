8cb096129859204a6d4b6293c88ae74dd8208f28
Dominique Castaneda <dominiquecastaneda@yahoo.com>
2022-07-19 16:34:31 -0500

    publish to github

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,9 +14,6 @@ function HomeScreen() {
            Decorativelily was created in 2022 after one of my son's became engaged. He requested my help in finding special ways to propose to his beautiful girlfriend at an out of state beach. Since I knew my hands were tied with not being able to take any of my handmade products on the plane, we had to look for event planners to help us plan it and thats when we came across a family owned company who set up picnics at the beach. With their help, we were able to make his proposal extra special. It was such a beautiful picture perfect setting and I knew that I wanted to do something similar here in the DFW area to help people plan out their memorable moments. My love of entertaining, crafting, sewing, and recently new woodworking hobby, came to play by me making my own products from picnic tables, sewing pillow covers, signs, etc. There was no question that this was a great business idea for me as I would love to help make great memories for my customers.
        </div>
       </div>
-      <div style={{alignItems: "center"}}>
-        <ReactPlayer url="https://www.youtube.com/shorts/L4hmH-tYhMc" />
-      </div>
       <div style={{textAlign: "center", color: "#ff69b4", marginTop: 50, fontSize: 50}}>
           Packages
           <div style={{textAlign: "center", color: "#ff69b4", fontSize: 25, marginTop: 10}}>
@@ -194,4 +191,7 @@ export default HomeScreen
 */
 
 /*
+ <div style={{alignItems: "center"}}>
+        <ReactPlayer url="https://www.youtube.com/shorts/L4hmH-tYhMc" />
+      </div>
 */
