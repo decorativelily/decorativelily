baa12029fa0c431db32cb2976bfa517fcea25b29
Dominique Castaneda <dominiquecastaneda@yahoo.com>
2022-07-20 12:19:03 -0500

    publish to github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import AboutScreen from './screens/AboutScreen';
 import BookingScreen from './screens/BookingScreen';
@@ -7,17 +7,76 @@ import FAQScreen from './screens/FAQScreen';
 import GalleryScreen from './screens/GalleryScreen';
 import HomeScreen from './screens/HomeScreen';
 import PackagesScreen from './screens/PackagesScreen';
-import SupportScreen from './screens/SupportScreen';
 
 function App() {
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
   return (
     <BrowserRouter>
     <div className="grid-container">
       <header className="row" style={{backgroundColor: "#ff69b4"}}>
-        <Link to="/" style={{color: "white", fontSize: 35}}>
+      <button
+              type="button"
+              className="open-sidebar"
+              onClick={() => setSidebarIsOpen(true)}
+            >
+              <i className="fa fa-bars"></i>
+            </button>
+        <div style={{marginRight: "42%"}}>
+        <Link to="/" style={{color: "white", fontSize: 35 }}>
         <img src='../images/decorativelilyalt.JPG' alt="Decorativelily Logo" height="35" width="75" style={{marginBottom: -5}}/> DecorativeLily
         </Link>
+        </div>
       </header>
+      <aside className={sidebarIsOpen ? 'open' : ''}>
+          <ul>
+            <li style={{marginLeft: 250}}>
+              <button
+                onClick={() => setSidebarIsOpen(false)}
+                className="close-sidebar"
+                type="button"
+              >
+                <i className="fa fa-close"></i>
+              </button>
+            </li>
+          </ul>
+          <div>
+            <ul>
+              <li style={{marginTop: -35}}>
+                <a href="/" style={{color: "#ff69b4"}}>Home</a>
+              </li>
+            </ul>
+            <ul>
+              <li style={{marginTop: 10}}>
+                <a href="/about" style={{color: "#ff69b4"}}>About</a>
+              </li>
+            </ul>
+            <ul>
+              <li style={{marginTop: 10}}>
+                <a href="/gallery" style={{color: "#ff69b4"}}>Gallery</a>
+              </li>
+            </ul>
+            <ul>
+              <li style={{marginTop: 10}}>
+                <a href="/package" style={{color: "#ff69b4"}}>Packages</a>
+              </li>
+            </ul>
+            <ul>
+              <li style={{marginTop: 10}}>
+                <a href="/booking" style={{color: "#ff69b4"}}>Book A Picnic</a>
+              </li>
+            </ul>
+            <ul>
+              <li style={{marginTop: 10}}>
+                <a href="/faq" style={{color: "#ff69b4"}}>FAQ</a>
+              </li>
+            </ul>
+            <ul>
+              <li style={{marginTop: 10}}>
+                <a href="/contact" style={{color: "#ff69b4"}}>Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </aside>
       <main>
       <div class="row center" style={{justifyContent: "space-evenly"}}>
       <ul>
@@ -55,11 +114,6 @@ function App() {
             <a href="/contact" style={{color: "#ff69b4"}}>Contact Us</a>
           </li>
         </ul>
-        <ul>
-          <li class="tablinks" style={{padding: 2}}>
-            <a href="/support" style={{color: "#ff69b4"}}>Support</a>
-          </li>
-        </ul>
         </div>
         <Routes>
           <Route path="/about" element={<AboutScreen />}></Route>
@@ -68,7 +122,6 @@ function App() {
           <Route path="/gallery" element={<GalleryScreen />}></Route>
           <Route path="/booking" element={<BookingScreen />}></Route>
           <Route path="/package" element={<PackagesScreen />}></Route>
-          <Route path="/support" element={<SupportScreen />}></Route>
           <Route path="/" element={<HomeScreen />}></Route>
         </Routes>
       </main>
