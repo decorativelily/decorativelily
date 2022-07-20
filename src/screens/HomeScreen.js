9c42d2ca1fe2bb7a3eb2f147813154f3401b0e6b
Dominique Castaneda <dominiquecastaneda@yahoo.com>
2022-07-19 22:43:03 -0500

    publish to github

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -147,13 +147,13 @@ function HomeScreen() {
             <form className="form" name="contact-form" method="POST" data-netlify="true" style={{}}>
                 <input type="hidden" name="form-name" value="contact-form" />
             <div style={{textAlign: "center"}}>
-                <input type="text" placeholder="Name" name="name" style={{color: "#ff69b4"}} />
+                <input type="text" placeholder="Name" name="name" style={{color: "#ff69b4"}} required/>
             </div>
             <div style={{textAlign: "center"}}>
-                <input type="text" placeholder="Email" name="email" style={{color: "#ff69b4"}}/>
+                <input type="text" placeholder="Email" name="email" style={{color: "#ff69b4"}} required/>
             </div>
             <div style={{textAlign: "center"}}>
-                <input type="text" placeholder="Phone Number" name="phoneNumber" style={{color: "#ff69b4"}}/>
+                <input type="text" placeholder="Phone Number" name="phoneNumber" style={{color: "#ff69b4"}} required/>
             </div>
             <div className="dropdown" style={{textAlign: "center"}}>
                    <select name="occassion" style={{color: "#ff69b4"}}>
@@ -165,16 +165,24 @@ function HomeScreen() {
                    </select>
             </div>
             <div style={{textAlign: "center"}}>
-                <input type="text" placeholder="Date" name="date" style={{color: "#ff69b4"}}/>
+                <input type="date" placeholder="Date" name="date" style={{color: "#ff69b4"}} required/>
             </div>
             <div style={{textAlign: "center"}}>
-                <input type="text" placeholder="Location" name="location" style={{color: "#ff69b4"}}/>
+                <input type="text" placeholder="Location" name="location" style={{color: "#ff69b4"}} required/>
             </div>
             <div style={{textAlign: "center"}}>
-                <input type="text" placeholder="Time" name="time" style={{color: "#ff69b4"}}/>
+                <input type="text" placeholder="Time" name="time" style={{color: "#ff69b4"}} required/>
             </div>
-            <div style={{textAlign: "center"}}>
-                <input type="text" placeholder="Guest Count" name="guestCount" style={{color: "#ff69b4"}}/>
+            <div className="dropdown" style={{textAlign: "center"}}>
+                   <select name="guestCount" style={{color: "#ff69b4"}}>
+                       <option name="">Guest Count</option>
+                       <option name="guestCount" value="2" style={{color: "#ff69b4"}}>2</option>
+                       <option name="guestCount" value="4" style={{color: "#ff69b4"}}>4</option>
+                       <option name="guestCount" value="6" style={{color: "#ff69b4"}}>6</option>
+                       <option name="guestCount" value="8" style={{color: "#ff69b4"}}>8</option>
+                       <option name="guestCount" value="10" style={{color: "#ff69b4"}}>10</option>
+                       <option name="guestCount" value="12" style={{color: "#ff69b4"}}>12</option>
+                   </select>
             </div>
             <div>
                 <label style={{color: "#ff69b4"}}>
@@ -185,7 +193,7 @@ function HomeScreen() {
                 </label>
             </div>
             <div style={{textAlign: "center"}}>
-                <textarea placeholder="Message" name="message" style={{color: "#ff69b4"}}></textarea>
+                <textarea placeholder="Message (Optional)" name="message" style={{color: "#ff69b4"}}></textarea>
             </div>
             <div style={{textAlign: "center"}}>
                 <button type="submit" style={{color: "#ff69b4"}}>Submit</button>
