b9cd832c75962b5c93de0e8090ceae08761b45a4
Dominique Castaneda <dominiquecastaneda@yahoo.com>
2022-07-20 11:02:38 -0500

    publish to github

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -160,7 +160,6 @@ function HomeScreen() {
             <div className="dropdown" style={{textAlign: "center"}}>
                    <select name="occassion" style={{color: "#ff69b4"}}>
                        <option name="">Occassion</option>
-                       <input name="occassion" value="" required></input>
                        <option name="occassion" value="anniversary" style={{color: "#ff69b4"}}>Anniversary</option>
                        <option name="occassion" value="birthday" style={{color: "#ff69b4"}}>Birthday</option>
                        <option name="occassion" value="graduation" style={{color: "#ff69b4"}}>Graduation</option>
@@ -179,7 +178,6 @@ function HomeScreen() {
             <div className="dropdown" style={{textAlign: "center"}}>
                    <select name="guestCount" style={{color: "#ff69b4"}}>
                        <option name="">Guest Count</option>
-                       <input name="guestCount" value="" required></input>
                        <option name="guestCount" value="2" style={{color: "#ff69b4"}}>2</option>
                        <option name="guestCount" value="4" style={{color: "#ff69b4"}}>4</option>
                        <option name="guestCount" value="6" style={{color: "#ff69b4"}}>6</option>
