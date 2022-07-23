b9d61e72239001f9b7546705774e70e41d105d2d
Dominique Castaneda <dominiquecastaneda@yahoo.com>
2022-07-23 12:48:30 -0500

    publish to github

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -4,9 +4,9 @@ function AboutScreen() {
     return (
         <div>
             <div style={{textAlign: "center", marginTop: 50, color: "#ff69b4", fontSize: 35}}>
-                Our Story
+                About
             <div style={{marginTop: 10, fontSize: 25}}>
-                DecorativeLily was created in 2022 after one of my son's became engaged. He requested my help in finding special ways to propose to his beautiful girlfriend at an out of state beach. Since I knew my hands were tied with not being able to take any of my handmade products on the plane, we had to look for event planners to help us plan it and thats when we came across a family owned company who set up picnics at the beach. With their help, we were able to make his proposal extra special. It was such a beautiful picture perfect setting and I knew that I wanted to do something similar here in the DFW area to help people plan out their memorable moments. My love of entertaining, crafting, sewing, and recently new woodworking hobby, came to play by me making my own products from picnic tables, sewing pillow covers, signs, etc. There was no question that this was a great business idea for me as I would love to help make great memories for my customers.
+                DecorativeLily was created in 2022 after my son became engaged. He requested my help in finding special ways to propose to his beautiful girlfriend at an out of state beach. Since I knew my hands were tied with not being able to take any of my handmade products on the plane, we had to look for event planners to help us plan it and thats when we came across a family owned company who set up picnics at the beach. With their help, we were able to make his proposal extra special. It was such a beautiful picture perfect setting and I knew that I wanted to do something similar here in the DFW area to help people plan out their memorable moments. My love of entertaining, crafting, sewing, and recently new woodworking hobby, came to play by me making my own products from picnic tables, sewing pillow covers, signs, etc. There was no question that this was a great business idea for me as I would love to help make great memories for my customers.
             </div>
             </div>
         </div>
